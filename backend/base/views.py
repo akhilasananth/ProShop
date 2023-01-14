382c5e5ebf96826c1bfa351c24980041cd923c57
Akhila Ananth <akhilasananth@gmail.com>
2023-01-13 22:12:22 -0500

    Backend setup

---
+++ backend/base/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
+
+# Create your views here.
+
+@api_view(['GET'])
+def get_routes(request):
+    return Response('Hello')
+
+@api_view(['GET'])
+def get_products(request):
+    return Response(products)
+
+@api_view(['GET'])
+def get_product(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    return Response(product)
